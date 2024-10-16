6aba5901c1aabe5cf991c94943fe62e5d2819d24
Pathogenbeats <0933490@my.scccd.edu>
2024-10-16 12:21:12 -0700

    Add files via upload
    Andrew Powell 10/16/2024

---
+++ cppNewParse.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\nWelcome To My Zoo Parsing Program\n";
+    cout <<   "---------------------------------\n";
+    string sampleInput = "4 year old female hyena, born in spring, tan color, 70 pounds, from Friguia Park, Tunisia";
+    cout << "\n" << sampleInput << "\n";
+
+    return 0;
+}
